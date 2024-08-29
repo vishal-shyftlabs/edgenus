18665966ed2cb436aadb1b0c4a6e3ef3a85c658b
deepadev1610 <deepadev1610@gmail.com>
2024-08-29 22:02:14 +0530

    WIP:Upload data to sheets

--- app/get-started/page.js
+++ app/get-started/page.js
@@ -6,10 +6,14 @@ import Footer from "../components/Footer";
 import Input from "../components/Input";
 import Stepper from "../components/Stepper";
 import TextArea from "../components/TextArea";
+import PrimaryButton from "../components/PrimaryButton";
+import axios from "axios";
 
 const page = (props) => {
   const [currentStep, setCurrentStep] = React.useState(0);
   const NUMBER_OF_STEPS = 5;
+  const SCRIPT_URL =
+    "https://script.google.com/macros/s/AKfycbyKmd4xJQFcKY6gsJXVIXN0ZXSj8eZ3iLnQDC5PH2OnXJweUjF29EflWbOACaBjpbJFCA/exec";
   return (
     <>
       <Header />
@@ -23,21 +27,18 @@ const page = (props) => {
         </div>
         <form>
           <div className="text-24 sm:text-28 font-bold">Business Info</div>
-
           <div className="text-label text-lg sm:text-24 mt-10">Identity</div>
-          <Input placeholder="Name" required className="flex" />
-          <Input placeholder="Email" required className="flex" />
-          <Input placeholder="Profession" required className="flex" />
-          <Input placeholder="Business Name*" required className="flex" />
+          <Input placeholder="Name" className="flex" />
+          <Input placeholder="Email" className="flex" />
+          <Input placeholder="Profession" className="flex" />
+          <Input placeholder="Business Name*" className="flex" />
           <TextArea placeholder="Business Description" className="flex" />
-
           <div className="absolute right-28 top-[26rem]">
             <Stepper
               currentStep={currentStep}
               numberOfSteps={NUMBER_OF_STEPS}
             />
           </div>
-
           <div className="text-label text-lg sm:text-24 mt-10">Address</div>
           <Input placeholder="Address line 1" className="flex" />
           <Input placeholder="Address line 2" className="flex" />
@@ -47,16 +48,26 @@ const page = (props) => {
             <Input placeholder="State" className="" />
           </div>
           <Input placeholder="Country" className="flex" />
-
           <div className="text-label text-lg sm:text-24 mt-10">
             Contact Info
           </div>
-          <Input
-            placeholder="Business Contact no.*"
-            required
-            className="flex"
+          <Input placeholder="Business Contact no.*" className="flex" />
+          <Input placeholder="Email*" className="flex" />
+
+          <PrimaryButton
+            onClick={(e) => {
+              e.preventDefault();
+              axios
+                .post(SCRIPT_URL, { name: "Deepa", email: "blah" })
+                .then((response) => {
+                  console.log("Success!", response.data);
+                })
+                .catch((error) => {
+                  console.error("Error!", error.message);
+                });
+            }}
+            text="Submit"
           />
-          <Input placeholder="Email*" required className="flex" />
         </form>
       </div>
       <Footer />
